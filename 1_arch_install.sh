00fbe7b4330c6babc9577082116a6e1f0e6aaf1e
munizio <48893316+munizio@users.noreply.github.com>
2019-08-21 22:04:54 -0500

    stripped down install scripts to bare essentials

--- 1_arch_install.sh
+++ 1_arch_install.sh
@@ -1,58 +1,45 @@
 #!/bin/bash
 
-encryption_passphrase=""
 root_password=""
-user_password=""
-hostname=""
 user_name=""
-continent_city=""
-swap_size="8"
+user_password=""
+hostname="cbnix"
+main_hdd="mmcblk0"
+efi_partition=$main_hdd"p1"
+root_partition=$main_hdd"p2"
+
 
 echo "Updating system clock"
 timedatectl set-ntp true
 
 echo "Creating partition tables"
-printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/nvme0n1
-printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/nvme0n1
+printf "n\n1\n4096\n+512M\nef00\nw\ny\n" | gdisk /dev/$main_hdd
+printf "n\n2\n\n\n8e00\nw\ny\n" | gdisk /dev/$main_hdd
 
 echo "Zeroing partitions"
-cat /dev/zero > /dev/nvme0n1p1
-cat /dev/zero > /dev/nvme0n1p2
+cat /dev/zero > /dev/$efi_partition
+cat /dev/zero > /dev/$root_partition
 
 echo "Building EFI filesystem"
-yes | mkfs.fat -F32 /dev/nvme0n1p1
+yes | mkfs.fat -F32 /dev/$efi_partition
 
-echo "Setting up cryptographic volume"
-printf "%s" "$encryption_passphrase" | cryptsetup -c aes-xts-plain64 -h sha512 -s 512 --use-random --type luks2 --label LVMPART luksFormat /dev/nvme0n1p2
-printf "%s" "$encryption_passphrase" | cryptsetup luksOpen /dev/nvme0n1p2 cryptoVols
+echo "Building filesystems for root "
+yes | mkfs.ext4 /dev/$root_partition
 
-echo "Setting up LVM"
-pvcreate /dev/mapper/cryptoVols
-vgcreate Arch /dev/mapper/cryptoVols
-lvcreate -L +"$swap_size"GB Arch -n swap
-lvcreate -l +100%FREE Arch -n root
-
-echo "Building filesystems for root and swap"
-yes | mkswap /dev/mapper/Arch-swap
-yes | mkfs.ext4 /dev/mapper/Arch-root
-
-echo "Mounting root/boot and enabling swap"
-mount /dev/mapper/Arch-root /mnt
-mkdir /mnt/boot
-mount /dev/nvme0n1p1 /mnt/boot
-swapon /dev/mapper/Arch-swap
+echo "Mounting root"
+mount /dev/$root_partition /mnt
 
 echo "Installing Arch Linux"
-yes '' | pacstrap /mnt base base-devel intel-ucode networkmanager wget linux-lts reflector apparmor
+yes '' | pacstrap /mnt base base-devel dialog wpa_supplicant
 
 echo "Generating fstab"
-genfstab -U /mnt >> /mnt/etc/fstab
+genfstab -U -p /mnt >> /mnt/etc/fstab
 
 echo "Configuring new system"
-arch-chroot /mnt /bin/bash <<EOF
+arch-chroot /mnt /bin/bash << EOF
 echo "Setting system clock"
-ln -fs /usr/share/zoneinfo/$continent_city /etc/localtime
-hwclock --systohc --localtime
+ln -fs /usr/share/zoneinfo/Americas/Chicago /etc/localtime
+hwclock --systohc --utc
 
 echo "Setting locales"
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
@@ -62,6 +49,9 @@ locale-gen
 echo "Setting hostname"
 echo $hostname > /etc/hostname
 
+echo "Setting hostfile"
+echo "127.0.0.1 localhost $hostname" > /etc/hosts
+
 echo "Setting root password"
 echo -en "$root_password\n$root_password" | passwd
 
@@ -69,98 +59,21 @@ echo "Creating new user"
 useradd -m -G wheel -s /bin/bash $user_name
 echo -en "$user_password\n$user_password" | passwd $user_name
 
-echo "Generating initramfs"
-sed -i 's/^HOOKS.*/HOOKS=(base udev keyboard autodetect modconf block keymap encrypt lvm2 resume filesystems fsck)/' /etc/mkinitcpio.conf
-sed -i 's/^MODULES.*/MODULES=(ext4 intel_agp i915)/' /etc/mkinitcpio.conf
-mkinitcpio -p linux-lts
-mkinitcpio -p linux
-
-echo "Setting up systemd-boot"
-bootctl --path=/boot install
-
-mkdir -p /boot/loader/
-touch /boot/loader/loader.conf
-tee -a /boot/loader/loader.conf << END
-default arch
-timeout 1
-editor 0
-END
-
-mkdir -p /boot/loader/entries/
-touch /boot/loader/entries/arch.conf
-tee -a /boot/loader/entries/arch.conf << END
-title ArchLinux
-linux /vmlinuz-linux
-initrd /intel-ucode.img
-initrd /initramfs-linux.img
-options cryptdevice=LABEL=LVMPART:cryptoVols root=/dev/mapper/Arch-root resume=/dev/mapper/Arch-swap apparmor=1 security=apparmor quiet rw
-END
-
-touch /boot/loader/entries/archlts.conf
-tee -a /boot/loader/entries/archlts.conf << END
-title ArchLinux
-linux /vmlinuz-linux-lts
-initrd /intel-ucode.img
-initrd /initramfs-linux-lts.img
-options cryptdevice=LABEL=LVMPART:cryptoVols root=/dev/mapper/Arch-root resume=/dev/mapper/Arch-swap apparmor=1 security=apparmor quiet rw
-END
-
-echo "Setting up Pacman hook for automatic systemd-boot updates"
-mkdir -p /etc/pacman.d/hooks/
-touch /etc/pacman.d/hooks/systemd-boot.hook
-tee -a /etc/pacman.d/hooks/systemd-boot.hook << END
-[Trigger]
-Type = Package
-Operation = Upgrade
-Target = systemd
-
-[Action]
-Description = Updating systemd-boot
-When = PostTransaction
-Exec = /usr/bin/bootctl update
-END
-
-echo "Enabling autologin"
-mkdir -p  /etc/systemd/system/getty@tty1.service.d/
-touch /etc/systemd/system/getty@tty1.service.d/override.conf
-tee -a /etc/systemd/system/getty@tty1.service.d/override.conf << END
-[Service]
-ExecStart=
-ExecStart=-/usr/bin/agetty --autologin $user_name --noclear %I $TERM
-END
-
-echo "Updating mirrors list"
-cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.BAK
-reflector --latest 200 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
-
-touch /etc/pacman.d/hooks/mirrors-update.hook
-tee -a /etc/pacman.d/hooks/mirrors-update.hook << END
-[Trigger]
-Operation = Upgrade
-Type = Package
-Target = pacman-mirrorlist
-
-[Action]
-Description = Updating pacman-mirrorlist with reflector
-When = PostTransaction
-Depends = reflector
-Exec = /bin/sh -c "reflector --latest 200 --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist"
-END
-
-echo "Enabling periodic TRIM"
-systemctl enable fstrim.timer
-
-echo "Enabling NetworkManager"
-systemctl enable NetworkManager
-
-echo "Enabling AppArmor"
-systemctl enable apparmor.service
-
 echo "Adding user as a sudoer"
 echo '%wheel ALL=(ALL) ALL' | EDITOR='tee -a' visudo
+
+echo "Installing GRUB"
+pacman -Sy grub efibootmgr
+mkdir /boot/efi
+mount /dev/$efi_partition /boot/efi
+grub-install --target=x86_64-efi --bootloader-id=GRUB --efi-directory=/boot/efi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
+
+mkdir /boot/efi/EFI/BOOT
+cp /boot/efi/EFI/GRUB/grubx64.efi /boot/efi/EFI/BOOT/BOOTX64.EFI
+
 EOF
 
 umount -R /mnt
-swapoff -a
 
 echo "ArchLinux is ready. You can reboot now!"
--- 2_arch_post_install.sh
+++ 2_arch_post_install.sh
@@ -1,191 +1,31 @@
 #!/bin/bash
 
 echo "Installing common packages"
-yes | sudo pacman -S linux-headers dkms xdg-user-dirs xorg-server-xwayland
-
-echo "Installing and configuring UFW"
-yes | sudo pacman -S ufw
-sudo systemctl enable ufw
-sudo systemctl start ufw
-sudo ufw enable
-sudo ufw default deny incoming
-sudo ufw default allow outgoing
-
-echo "Installing and enabling TLP"
-yes | sudo pacman -S tlp tlp-rdw
-sudo systemctl enable tlp.service
-sudo systemctl enable tlp.service
-sudo systemctl start tlp.service
-sudo systemctl start tlp.service
-sudo systemctl enable NetworkManager-dispatcher.service
-sudo systemctl mask systemd-rfkill.service
-sudo systemctl mask systemd-rfkill.socket
+yes | sudo pacman -S xorg-server-xwayland
 
 echo "Installing common applications"
-echo -en "1\nyes" | sudo pacman -S firefox chromium keepassxc git openssh neovim links alacritty upower htop powertop p7zip ripgrep
-
-echo "Installing office applications"
-yes | sudo pacman -S tumbler evince thunderbird 
-
-#echo "Installing image editing applications"
-#yes | sudo pacman -S gimp inkscape
+echo -en "1\nyes" | sudo pacman -S chromium git openssh htop curl wget
 
 echo "Installing fonts"
 yes | sudo pacman -S ttf-droid ttf-opensans ttf-dejavu ttf-liberation ttf-hack ttf-fira-code noto-fonts gsfonts powerline-fonts
-
-echo "Installing Node.js LTS"
-yes | sudo pacman -S nodejs-lts-dubnium npm
-
-echo "Changing default npm directory"
-mkdir ~/.npm-global
-npm config set prefix '~/.npm-global'
-touch ~/.profile
-tee ~/.profile << END
-export PATH=~/.npm-global/bin:$PATH
-END
-source ~/.profile
-
-echo "Configuring golang with LSP server"
-yes | sudo pacman -S go go-tools
-mkdir -p ~/go/src
-GO111MODULE=on go get golang.org/x/tools/gopls@latest
-
-#echo "Installing NVM and Node.js LTS"
-#git clone https://aur.archlinux.org/nvm.git
-#cd nvm
-#yes | makepkg -si
-#cd ..
-#rm -rf nvm
-#source /usr/share/nvm/init-nvm.sh
-#nvm install --lts=dubnium
-
-#echo "Adding Node.js provider for neovim"
-#npm install -g neovim
-
-echo "Adding Python 3 provider for neovim"
-yes | sudo pacman -S python-pip
-pip install --user --upgrade pynvim
-
-echo "Configuring neovim"
-mkdir -p ~/.config/nvim
-wget -P ~/.config/nvim https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/nvim/init.vim
-wget -P ~/.config/nvim https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/nvim/coc-settings.json
-
-echo "Installing vim-plug"
-curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim +'PlugInstall --sync' +qa
-
-echo "Installing neovim language extensions"
-nvim +'CocInstall -sync coc-json' +qall
-nvim +'CocInstall -sync coc-tsserver' +qall
-nvim +'CocInstall -sync coc-html' +qall
-nvim +'CocInstall -sync coc-css' +qall
-nvim +'CocInstall -sync coc-python' +qall
-nvim +'CocInstall -sync coc-svg' +qall
-nvim +'CocInstall -sync coc-eslint' +qall
-nvim +'CocInstall -sync coc-prettier' +qall
-#nvim +'silent :GoInstallBinaries' +qall
-
-echo "Installing VS Code + theme + icons"
-yes | sudo pacman -S code
-code --install-extension pkief.material-icon-theme
-code --install-extension equinusocio.vsc-material-theme
-
-echo "Installing and setting zsh"
-yes | sudo pacman -S zsh zsh-theme-powerlevel9k
-chsh -s /bin/zsh
-wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc
-wget -P ~/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zprofile
-mkdir -p ~/.zshrc.d
-wget -P ~/.zshrc.d https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc.d/environ.zsh
-wget -P ~/.zshrc.d https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/zsh/.zshrc.d/wayland.zsh
-
-echo "Setting up GTK theme and installing dependencies"
-yes | sudo pacman -S gtk-engine-murrine gtk-engines
-git clone https://github.com/vinceliuice/Qogir-theme.git
-cd Qogir-theme
-sudo mkdir -p /usr/share/themes
-sudo ./install.sh -d /usr/share/themes
-cd ..
-rm -rf Qogir-theme
-
-echo "Setting up icon theme"
-git clone https://github.com/vinceliuice/Qogir-icon-theme.git
-cd Qogir-icon-theme
-sudo mkdir -p /usr/share/icons
-sudo ./install.sh -d /usr/share/icons
-cd ..
-rm -rf Qogir-icon-theme
 
 echo "Installing Material Design icons"
 sudo mkdir -p /usr/share/fonts/TTF/
 sudo wget -P /usr/share/fonts/TTF/ https://raw.githubusercontent.com/Templarian/MaterialDesign-Webfont/master/fonts/materialdesignicons-webfont.ttf
 
 echo "Installing sway and additional packages"
-yes | sudo pacman -S sway swaylock swayidle waybar wl-clipboard pulseaudio pavucontrol rofi slurp grim thunar mousepad nnn light feh qalculate-gtk
+yes | sudo pacman -S sway swaylock swayidle waybar wl-clipboard pulseaudio pavucontrol alsa-utils rofi light termite
 mkdir -p ~/.config/sway
 wget -P ~/.config/sway/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/sway/config
-mkdir -p ~/Pictures/screenshots
-
-echo "Enabling auto-mount for thunar"
-yes | sudo pacman -S gvfs thunar-volman
-
-echo "Setting wallpaper"
-wget -P ~/Pictures/ https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/wallpaper/omar-g-garnica-6gdqWFolkC4-unsplash.jpg
 
 echo "Ricing waybar"
 mkdir -p ~/.config/waybar
 wget -P ~/.config/waybar https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/waybar/config
 wget -P ~/.config/waybar https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/waybar/style.css
 
-echo "Ricing Alacritty"
-mkdir -p ~/.config/alacritty
-wget -P ~/.config/alacritty https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/alacritty/alacritty.yml
-
 echo "Ricing rofi"
 mkdir -p ~/.config/rofi
 wget -P ~/.config/rofi https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/rofi/base16-material-darker.rasi
 wget -P ~/.config/rofi https://raw.githubusercontent.com/exah-io/minimal-arch-linux/master/configs/rofi/config
-
-echo "Blacklisting bluetooth"
-sudo touch /etc/modprobe.d/nobt.conf
-sudo tee /etc/modprobe.d/nobt.conf << END
-blacklist btusb
-blacklist bluetooth
-END
-sudo mkinitcpio -p linux-lts
-sudo mkinitcpio -p linux
-
-echo "Increasing the amount of inotify watchers"
-echo fs.inotify.max_user_watches=524288 | sudo tee /etc/sysctl.d/40-max-user-watches.conf && sudo sysctl --system
-
-echo "Enabling audio power saving"
-sudo touch /etc/modprobe.d/audio_powersave.conf
-echo "options snd_hda_intel power_save=1" | sudo tee /etc/modprobe.d/audio_powersave.conf
-
-echo "Installing and configuring Firejail"
-yes | sudo pacman -S firejail
-sudo apparmor_parser -r /etc/apparmor.d/firejail-default
-sudo firecfg
-sudo touch /etc/pacman.d/hooks/firejail.hook
-sudo tee -a /etc/pacman.d/hooks/firejail.hook << END
-[Trigger]
-Type = File
-Operation = Install
-Operation = Upgrade
-Operation = Remove
-Target = usr/bin/*
-Target = usr/local/bin/*
-Target = usr/share/applications/*.desktop
-[Action]
-Description = Configure symlinks in /usr/local/bin based on firecfg.config...
-When = PostTransaction
-Depends = firejail
-Exec = /bin/sh -c 'firecfg &>/dev/null'
-END
-
-echo "Granting internet access again to VSCode in Firejail profile"
-sudo sed -i 's/net none/#net none/g' /etc/firejail/code.profile
-sudo firecfg
 
 echo "Your setup is ready. You can reboot now!"
